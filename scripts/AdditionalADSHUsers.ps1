317fceee665508f302c990135c7c341209060b9a
Kent Ickler <29710634+Relkci@users.noreply.github.com>
2025-08-12 06:46:55 -0600

    Update AdditionalADSHUsers.ps1

--- scripts/AdditionalADSHUsers.ps1
+++ scripts/AdditionalADSHUsers.ps1
@@ -123,3 +123,29 @@ New-ADUser -Name "$pwMgrFirst $pwMgrLast" `
 Add-ADGroupMember -Identity "SG-PasswordManagers" -Members $pwMgrSam
 
 # (Control path violation: pwMgrSam is not in Helpdesk, but can reset passwords for all users)
+
+
+# Low Privilege Service Account
+New-ADUser -Name "svc_ADSHLowPriv" `
+    -SamAccountName "svc_ADSHLowPriv" `
+    -UserPrincipalName "svc_ADSHLowPriv@adshclass.com" `
+    -DisplayName "ADSH Low Privilege Service Account" `
+    -Title "Service Account - Low Privilege" `
+    -Department "IT" `
+    -Path "OU=IT,OU=ADSHMedical,DC=adshclass,DC=com" `
+    -AccountPassword (ConvertTo-SecureString "S3rv1c3LowP@ss!" -AsPlainText -Force) `
+    -Enabled $true
+
+# High Privilege Service Account
+New-ADUser -Name "svc_ADSHHighPriv" `
+    -SamAccountName "svc_ADSHHighPriv" `
+    -UserPrincipalName "svc_ADSHHighPriv@adshclass.com" `
+    -DisplayName "ADSH High Privilege Service Account" `
+    -Title "Service Account - High Privilege" `
+    -Department "IT" `
+    -Path "OU=IT,OU=ADSHMedical,DC=adshclass,DC=com" `
+    -AccountPassword (ConvertTo-SecureString "S3rv1c3HighP@ss!" -AsPlainText -Force) `
+    -Enabled $true
+
+# Add high privilege service account to privileged group
+Add-ADGroupMember -Identity "SG-Privileged-Admins" -Members "svc_ADSHHighPriv"
