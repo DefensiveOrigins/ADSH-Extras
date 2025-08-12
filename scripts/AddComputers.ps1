5a4723d5a76314b91fb6033d42f97b1f8083570d
Kent Ickler <29710634+Relkci@users.noreply.github.com>
2025-08-12 08:53:41 -0600

    Update AddComputers.ps1

--- scripts/AddComputers.ps1
+++ scripts/AddComputers.ps1
@@ -1,24 +1,22 @@
-# Create 100 computer accounts, evenly distributed across department OUs
+# Create 20 computer accounts in each department OU using New-ADComputer
 
 $departments = @(
-    @{Name="Administration"; Abbr="ADM"; OU="OU=Administration,OU=ADSHMedical,DC=adshclass,DC=com"},
-    @{Name="ClinicalServices"; Abbr="CLN"; OU="OU=ClinicalServices,OU=ADSHMedical,DC=adshclass,DC=com"},
-    @{Name="IT"; Abbr="IT"; OU="OU=IT,OU=ADSHMedical,DC=adshclass,DC=com"},
-    @{Name="BillingAndRecords"; Abbr="BIL"; OU="OU=BillingAndRecords,OU=ADSHMedical,DC=adshclass,DC=com"},
-    @{Name="FacilitiesAndSupport"; Abbr="FAC"; OU="OU=FacilitiesAndSupport,OU=ADSHMedical,DC=adshclass,DC=com"}
+    @{Abbr="ADM"; OU="OU=Administration,OU=ADSHMedical,DC=adshclass,DC=com"},
+    @{Abbr="CLN"; OU="OU=ClinicalServices,OU=ADSHMedical,DC=adshclass,DC=com"},
+    @{Abbr="IT";  OU="OU=IT,OU=ADSHMedical,DC=adshclass,DC=com"},
+    @{Abbr="BIL"; OU="OU=BillingAndRecords,OU=ADSHMedical,DC=adshclass,DC=com"},
+    @{Abbr="FAC"; OU="OU=FacilitiesAndSupport,OU=ADSHMedical,DC=adshclass,DC=com"}
 )
 
-for ($i=1; $i -le 100; $i++) {
-    $deptIndex = ($i - 1) % $departments.Count
-    $dept = $departments[$deptIndex]
-    $pcNum = "{0:D3}" -f [math]::Ceiling($i / $departments.Count + $deptIndex * 20)
-    $compName = "$($dept.Abbr)-PC$pcNum"
-    New-ADComputer -Name $compName `
-        -SamAccountName $compName `
-        -Path $dept.OU `
-        -Enabled $true
+foreach ($dept in $departments) {
+    for ($i=1; $i -le 20; $i++) {
+        $pcName = "$($dept.Abbr)-PC{0:D2}" -f $i
+        New-ADComputer -Name $pcName `
+            -SamAccountName $pcName `
+            -Path $dept.OU `
+            -Enabled $true
+    }
 }
-
 
 # Create 5 IT department computers trusted for delegation (not domain controllers)
 
