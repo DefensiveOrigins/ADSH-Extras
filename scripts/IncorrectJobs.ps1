3da415204434afc92da487a3997e40742425e467
Kent Ickler <29710634+Relkci@users.noreply.github.com>
2025-08-12 06:58:10 -0600

    Create IncorrectJobs.ps1

---
+++ scripts/IncorrectJobs.ps1
@@ -0,0 +1,16 @@
+# Incorrectly add users to groups that do not match their job titles
+
+# Add an HR Specialist to the Doctor group
+Add-ADGroupMember -Identity "SG-Doctor" -Members emily.carter
+
+# Add a Security Analyst to the Janitor group
+Add-ADGroupMember -Identity "SG-Janitor" -Members isabella.chen
+
+# Add a Medical Assistant to the SysAdmin group
+Add-ADGroupMember -Identity "SG-SysAdmin" -Members ethan.brooks
+
+# Add a Janitor to the Nurse group
+Add-ADGroupMember -Identity "SG-Nurse" -Members william.ortiz
+
+# Add an Executive Assistant to the Maintenance group
+Add-ADGroupMember -Identity "SG-Maintenance" -Members noah.james
