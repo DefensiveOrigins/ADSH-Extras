d898c03f2363009dc831a2b6eec85b92506a01f1
Kent Ickler <29710634+Relkci@users.noreply.github.com>
2025-08-12 20:34:39 -0600

    Update ShortcutLab2.ps1

--- scripts/ShortcutLab2.ps1
+++ scripts/ShortcutLab2.ps1
@@ -1,3 +1,3 @@
 Add-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
-Install-ADDSForest -DomainName "ADSHClass.com" -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2025" -DomainNetbiosName "ADSHClass" -ForestMode "Win2025"  -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$True -SysvolPath "C:\Windows\SYSVOL" -Force:$true
+Install-ADDSForest -DomainName "ADSHClass.com" -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2025" -DomainNetbiosName "ADSHClass" -ForestMode "Win2025"  -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$True -SysvolPath "C:\Windows\SYSVOL" -Force:$true -SafeModeAdministratorPassword (ConvertTo-SecureString "ADSHAdmin12345!" -AsPlainText -Force)
 Restart-Computer
