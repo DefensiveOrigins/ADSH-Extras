e0569cc0744b8ec4f8b18139b1c278758c25aa22
Kent Ickler <29710634+Relkci@users.noreply.github.com>
2025-08-13 01:52:37 -0600

    Update ShortcutLab9.1.ps1

--- scripts/ShortcutLab9.1.ps1
+++ scripts/ShortcutLab9.1.ps1
@@ -6,11 +6,11 @@ $ProgressPreference = 'Continue'
 Expand-Archive ADExplorer.zip
 Remove-Item ADExplorer.zip
 New-Item -ItemType Directory -Path "C:\ADSH\ADExplorer-Snapshots" -Force > $null
-ls ADExplorer
+Get-ChildItem  C:\ADSH\ADExplorer\ | Select-Object Name, Length
 
 cd c:\ADSH\AdExplorer 
 ./AdExplorer64.exe -snapshot "ADSHclass.com" c:\ADSH\ADExplorer-Snapshots\ADSHClass.com.1.dat  -accepteula 
-ls C:\ADSH\ADExplorer-Snapshots\
+Get-ChildItem  C:\ADSH\ADExplorer-Snapshots\ | Select-Object Name, Length
 
 cd c:\ADSH
 DSADD user -upn Jolly.Rogers@ADSHclass.com "cn=Jolly.Rogers,CN=Users,DC=ADSHClass,DC=com" -fn "Jolly" -ln "Rogers" -disabled no -display "Jolly Rogers" -desc "Ranch Manager" -office "Yellowstone" -title "Ranch Manager" -company "Branch Ranch" -PWD "Badpass215613!!3"
@@ -18,7 +18,7 @@ Set-ADUser -Identity Jolly.Rogers -PasswordNeverExpires $true
 
 cd c:\ADSH\AdExplorer 
 ./AdExplorer64.exe -snapshot "ADSHclass.com" c:\ADSH\ADExplorer-Snapshots\ADSHClass.com.2.dat  -accepteula
-ls C:\ADSH\ADExplorer-Snapshots\
+Get-ChildItem  C:\ADSH\ADExplorer-Snapshots\ | Select-Object Name, Length
 
 cd c:\ADSH\AdExplorer
 ./AdExplorer64.exe /accepteula
