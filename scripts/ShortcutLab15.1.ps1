6afbec29f259a754a865d62ca9c23b242edc70a8
Relkci <29710634+Relkci@users.noreply.github.com.>
2025-08-17 02:43:04 -0600

    maq

--- scripts/ShortcutLab15.1.ps1
+++ scripts/ShortcutLab15.1.ps1
@@ -1,4 +1,5 @@
-Get-ADDomain -Identity "adshclass.com" | Select-Object ms-DS-MachineAccountQuota
-Set-ADDomain -Identity "adshclass.com" -Replace @{'ms-DS-MachineAccountQuota'=0}
+Get-ADObject ((Get-ADDomain).distinguishedname) -Properties ms-DS-MachineAccountQuota
+Set-ADDomain -Identity "adshclass.com" -Replace @{'ms-DS-MachineAccountQuota'=0}}
+Get-ADObject ((Get-ADDomain).distinguishedname) -Properties ms-DS-MachineAccountQuota
 New-ADGroup -Name "SEC_ComputerJoiners" -GroupScope Global -Path "OU=Groups,DC=adshclass,DC=com"
 dsacls "OU=Workstations,DC=adshclass,DC=com" /G "adshclass\SEC_ComputerJoiners:CC;computer"
