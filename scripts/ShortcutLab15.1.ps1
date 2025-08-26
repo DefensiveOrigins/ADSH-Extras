83a269448413e1929ccb3fb4f61caaf728242180
Relkci <29710634+Relkci@users.noreply.github.com.>
2025-08-26 03:32:38 -0600

    sc15.1

--- scripts/ShortcutLab15.1.ps1
+++ scripts/ShortcutLab15.1.ps1
@@ -2,6 +2,14 @@ Get-ADObject ((Get-ADDomain).distinguishedname) -Properties ms-DS-MachineAccount
 Set-ADDomain -Identity "adshclass.com" -Replace @{'ms-DS-MachineAccountQuota'=0}}
 Get-ADObject ((Get-ADDomain).distinguishedname) -Properties ms-DS-MachineAccountQuota
 New-ADGroup -Name "SEC_ComputerJoiners" -GroupScope Global -Path "OU=Groups,DC=adshclass,DC=com"
-Write-Output "[+] DACLS" 
+Write-Output "[*] Get MAQ" 
+Get-ADObject ((Get-ADDomain).distinguishedname) -Properties ms-DS-MachineAccountQuota
+Write-Output "[*] Set MAQ"
+Set-ADDomain -Identity "adshclass.com" -Replace @{'ms-DS-MachineAccountQuota'=0}
+Write-Output "[*] Get MAQ" 
+Get-ADObject ((Get-ADDomain).distinguishedname) -Properties ms-DS-MachineAccountQuota
+Write-Output "[+] Create Group"
+New-ADGroup -Name "SEC_ComputerJoiners" -GroupScope Global -Path "OU=ADSHMedical,DC=adshclass,DC=com"
+Write-Output "[+] Delegate" 
 dsacls "OU=Workstations,DC=adshclass,DC=com" /G "adshclass\SEC_ComputerJoiners:CC;computer"
 Write-Output "[!] Done" 
