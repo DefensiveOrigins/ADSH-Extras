839e2de6df1a12c8720ed35d0362214673b5fa8c
Relkci <29710634+Relkci@users.noreply.github.com.>
2025-08-26 14:15:45 -0600

    CheckContext

---
+++ scripts/CheckContext.ps1
@@ -0,0 +1,33 @@
+param (
+    [string]$HostName
+)
+
+# Check if running as Administrator
+$adminCheck = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$isAdmin = (New-Object System.Security.Principal.WindowsPrincipal $adminCheck).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+
+if (-not $isAdmin) {
+    Write-Host "`n`nWARN: This script must be run as an Administrator. Exiting...`n`n"
+    pause
+    Stop-Process -Id $PID
+}
+
+
+if (-not $HostName) {
+    Write-Host "No hostname provided. Exiting..."
+    exit
+}
+
+if ($env:COMPUTERNAME -ne $HostName) {
+    Write-Host "`n`nWARN: Wrong system. Expected: $HostName, Found: $env:COMPUTERNAME. Exiting...`n`n"
+    pause
+    Stop-Process -Id $PID
+}
+
+if ($env:USERNAME -ne "ADSHClass" -or $env:USERDOMAIN -ne "ADSHClass") {
+    Write-Host "`n`nWARN: Wrong user. Expected: ADSHClass\Administrator, Found: $env:USERDOMAIN\$env:USERNAME. Exiting...`n`n"
+    pause
+    Stop-Process -Id $PID
+}
+
+Write-Host "`n`nINFO: Running on correct system: $env:COMPUTERNAME as $env:USERDOMAIN\$env:USERNAME with Administrator privileges.`n`n"
